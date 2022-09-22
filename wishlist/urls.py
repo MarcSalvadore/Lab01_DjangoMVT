f32513ae6bb2814b425588b9d5d0c6742c0bbe89
MarcoSalvadore <marcsalvadore2@gmail.com>
2022-09-22 13:41:55 +0700

    COMMIT FINISH

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,9 @@ from wishlist.views import show_json
 from wishlist.views import show_xml
 from wishlist.views import show_wishlist
 from wishlist.views import show_json_by_id
+from wishlist.views import register #Import fungsi register dari views
+from wishlist.views import login_user # Import fungsi login user
+from wishlist.views import logout_user # Import fungsi logout user
 
 app_name = 'wishlist'
 
@@ -11,4 +14,7 @@ urlpatterns = [
     path('xml/', show_xml, name='http_response'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'), #Path untuk login ke fungsi register yang dibuat dalam views
+    path('login/', login_user, name='login'), # Path untuk fungsi login user
+    path('logout/', logout_user, name='logout'), # Path untuk fungsi logout user
 ]
